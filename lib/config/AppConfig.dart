68e56ef7ea28b4f55f10999202a36b0cc0fdabc0
Đinh Văn Phi <dinhvanphi01478965@gmail.com>
2025-08-18 22:31:46 +0700

    change ngrok and set up ios devices url

--- lib/config/AppConfig.dart
+++ lib/config/AppConfig.dart
@@ -7,18 +7,18 @@ class AppConfig {
     if (kDebugMode) {
       if (Platform.isAndroid) {
         // Sử dụng ngrok URL cho thiết bị Android khi debug
-        return 'https://943dbe43e5d3.ngrok-free.app';
+        return 'https://52b9e04a5ef9.ngrok-free.app';
       } else if (Platform.isIOS) {
         // Sử dụng ngrok URL cho thiết bị iOS khi debug
-        return 'https://943dbe43e5d3.ngrok-free.app';
+        return 'http://localhost:3000';
       }
     }
     // Đường dẫn production hoặc debug trên thiết bị thật
-    return 'https://943dbe43e5d3.ngrok-free.app';
+    return 'https://52b9e04a5ef9.ngrok-free.app';
   }
   
   // URL thanh toán VNPay
-  static const String vnpayApiUrl = 'https://943dbe43e5d3.ngrok-free.app';
+  static const String vnpayApiUrl = 'https://52b9e04a5ef9.ngrok-free.app';
   static const String vnpayReturnUrl = vnpayApiUrl + '/api/payment/vnpay/return';
 
   // API Endpoints
